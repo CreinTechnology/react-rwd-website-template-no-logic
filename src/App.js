ac8944f96eee58da8e259ad994856ed0f8a12903
Paweł Czerniak <kontakt@crein.pl>
2022-03-15 00:05:57 +0100

    `Navbarz & `Image` styles improved

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
+import Container from './components/Container'
 import Image from './components/Image'
 import Navbar from './components/Navbar'
+import Typography from './components/Typography'
+import Paragraph from './components/Paragraph'
+import TextField from './components/TextField'
+import Button from './components/Button'
 
 import circles from './images/circles.png'
 
@@ -19,6 +24,42 @@ export const App = () => {
       />
 
       <Navbar />
+
+      <Container
+        className={classes.textWrapper}
+      >
+        <Typography
+          className={classes.textHeader}
+        >
+          Best{' '}
+          <Typography
+            className={classes.textHeader__bold}
+          >
+            Solution Provider {' '}
+          </Typography>
+          For Your{' '}
+          <Typography
+            className={classes.textHeader__bold}
+          >
+            Business.
+          </Typography>
+        </Typography>
+
+        <Paragraph
+          className={classes.textParagraph}
+        >
+          Vivamus ut arcu dignissim massa pulvinar scelerisque a in purus. Vivamus sit amet ex dui. In tortor massa, sollicitudin id ligula nec, vulputate viverra purus.
+        </Paragraph>
+
+        <TextField
+          className={classes.formField}
+          placeholder={'Name@company.com'}
+        />
+        <Button
+          className={classes.formButton}
+          buttonLabel={'Try for free'}
+        />
+      </Container>
 
     </div>
   )
