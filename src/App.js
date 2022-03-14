23772357a114366fef3337d08415e5ba69a7c8b7
Paweł Czerniak <kontakt@crein.pl>
2022-03-14 14:59:23 +0100

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// Every React-component file have to began from this line below
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div>
+      Crein App
+    </div>
+  )
+}
+export default App
